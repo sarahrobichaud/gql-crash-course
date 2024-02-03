6e3aee7f16b7b2284892d13bbeb6ec599c373fb2
Sarah Robichaud <robichaudsarah@outlook.com>
2024-02-03 14:34:49 -0400

    Added resolvers

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
-import { typeDefs } from "./schema";
+import { typeDefs } from "./schema.js";
 
+import data from "./_data.js";
 // Server setup
 const PORT = process.env.PORT || 4000;
 
+const resolvers = {
+  Query: {
+    games() {
+      return data.games;
+    },
+    reviews() {
+      return data.reviews;
+    },
+    authors() {
+      return data.authors;
+    },
+  },
+};
+
 const server = new ApolloServer({
   typeDefs,
+  resolvers,
 });
 
 const { url } = await startStandaloneServer(server, {
